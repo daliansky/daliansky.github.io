8fc38d2bc6ecb3b476fe70c64d8d15e3e215fffc
sky <sky@skydeXiaoXinPro.local>
2020-01-06 19:49:21 +0800

    Site updated: 2020-01-06 19:49:21

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Sun Jan 05 2020 18:54:03 GMT+0800 (中国标准时间)
+// build time:Mon Jan 06 2020 19:49:17 GMT+0800 (中国标准时间)
 $(document).ready(function(){var t=$(".sidebar-inner");var e=CONFIG.sidebar.offset?CONFIG.sidebar.offset:12;function i(){return $(".header-inner").height()+e}function r(){var t=$(".footer-inner");var e=t.outerHeight(true)-t.outerHeight();var i=t.outerHeight(true)+e;return i}function a(){var a=i();var f=r();var n=$("#sidebar").height()+NexT.utils.getSidebarb2tHeight();var o=$("#content").height();if(a+n<o){t.affix({offset:{top:a-e,bottom:f}});t.affix("checkPosition")}$("#sidebar").css({"margin-top":a,"margin-left":"auto"})}function f(){$(window).off(".affix");t.removeData("bs.affix").removeClass("affix affix-top affix-bottom");a()}function n(){var t=window.matchMedia("(min-width: 992px)");t.addListener(function(t){if(t.matches){f()}})}a();n()});
 //rebuild by neat 
--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Sun Jan 05 2020 18:54:03 GMT+0800 (中国标准时间)
+// build time:Mon Jan 06 2020 19:49:17 GMT+0800 (中国标准时间)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]===o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
