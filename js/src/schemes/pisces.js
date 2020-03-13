44bb53ec7c029d74387af0e9bc817dbe27c82f59
DalianSky <daliansky2008@gmail.com>
2020-03-13 14:07:25 +0800

    Site updated: 2020-03-13 14:07:24

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Thu Mar 12 2020 20:12:28 GMT+0800 (GMT+08:00)
+// build time:Fri Mar 13 2020 14:07:22 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var t=$(".sidebar-inner");var e=CONFIG.sidebar.offset?CONFIG.sidebar.offset:12;function i(){return $(".header-inner").height()+e}function r(){var t=$(".footer-inner");var e=t.outerHeight(true)-t.outerHeight();var i=t.outerHeight(true)+e;return i}function a(){var a=i();var f=r();var n=$("#sidebar").height()+NexT.utils.getSidebarb2tHeight();var o=$("#content").height();if(a+n<o){t.affix({offset:{top:a-e,bottom:f}});t.affix("checkPosition")}$("#sidebar").css({"margin-top":a,"margin-left":"auto"})}function f(){$(window).off(".affix");t.removeData("bs.affix").removeClass("affix affix-top affix-bottom");a()}function n(){var t=window.matchMedia("(min-width: 992px)");t.addListener(function(t){if(t.matches){f()}})}a();n()});
 //rebuild by neat 
--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Thu Mar 12 2020 20:12:28 GMT+0800 (GMT+08:00)
+// build time:Fri Mar 13 2020 14:07:22 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]===o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
