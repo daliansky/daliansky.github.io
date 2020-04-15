4b91ff4dce6eaaeb17f557ca50b4a6b689c7295f
DalianSky <daliansky2008@gmail.com>
2020-04-15 20:56:39 +0800

    Site updated: 2020-04-15 20:56:38

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Tue Apr 14 2020 11:02:24 GMT+0800 (GMT+08:00)
+// build time:Wed Apr 15 2020 20:56:35 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]===o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
